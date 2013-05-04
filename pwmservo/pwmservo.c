d3cf93afc5c0e4c463c9a17ba98f572c0d5a8668
Christopher Woodall <chris.j.woodall@gmail.com>
2013-05-04 17:54:09 -0400

    Added comments to pwmservo.c

--- pwmservo/pwmservo.c
+++ pwmservo/pwmservo.c
@@ -1,24 +1,16 @@
 /* PWMServo - PWM Controlled Servo Driver for the PXA270 (Gumstix Verdex)
  * Author: Christopher Woodall <cwoodall@bu.edu>
+ * Date: Thu May 2, 2013
  *
  * Base PWM Frequency is 13MHz, but can be divided using the PWM_PWCTRLx and
  * PWM_PERVALx to set the prescaler and period length. With both set to the
  * largest value the PWM period can be is 5ms. This is a faster period 
  * than the expected RC Servo PWM frame period of 20ms. The standard is forgiving
  * of 5ms period, but there is a high power draw and it SEVERLY reduces the servos
- * lifetime.
- *
- *
- * ONE: A solution is to use a timer to turn the outgoing servo pulses on 
- *      and off at some frequency near 20ms. This just mitigates some of the harsher 
- *      problems of RC servo drivers.
- *
- *
- * TWO: A solution is to use the internal OS Timer Match 1 with a period of ~5ms to
- *      toggle the PWM frequency on and off (on for 1 off for 4) to reduce it to a
- *      a 20 ms PWM cycle. There may be missing pulses or extra pulses occasionally,
- *      but on average we should have a base 20ms period RC Servo frame and the 
- *      occasional extra pulse should not provide any major problems.
+ * lifetime. To solve this problem a OS Timer on Match 1 is used to help toggle the
+ * PWM signal on and off. The scheme is to let about 4 signals through every 100ms.
+ * This scheme has been shown to work in situations with low torque requirements and
+ * provides stable motion.
  */
 
 /* Necessary includes for device drivers */
@@ -47,10 +39,10 @@
 #define TOGGLE_TICK 80000
  // @TODO figure out resolution maths
 #define PWMSERVO_MAJOR 61
+
 /** Define module metadata */
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Christopher J. Woodall <chris.j.woodall@gmail.com>");
-//MODULE_DESCRIPTION("Up and down LED counter w/ GPIO and Interrupts (irq).");
 
 /** Define pwmservo file ops */
 /* Define mytimer file operator function headers (open, release, read, write) */
@@ -75,9 +67,11 @@ static void pwmservo_exit(void);
 module_init(pwmservo_init);
 module_exit(pwmservo_exit);
 
+// Setup servo value global variables.
 uint32_t servo_val0 = 0;
 uint32_t servo_val1 = 0;
 uint32_t servo_cycles = 0;
+
 /**
  * static irqreturn_t servo_timer_handler(int, void) 
  *
@@ -90,17 +84,15 @@ uint32_t servo_cycles = 0;
 static irqreturn_t servo_timer_handler(int irq, void *dev_id) {
     /*    printk(KERN_ALERT "HANDLING!");
           printk(KERN_ALERT "%i", OSCR);*/
-        int next_match;
-
-    // Setup next match
-        //    write_seqlock(&xtime_lock);
+    int next_match;
+    
     do {
         OSSR = OSSR_M1;  /* Clear match on timer 1 */
         next_match = (OSMR1 += TOGGLE_TICK);
-        //    OSMR1 = OSCR + TOGGLE_TICK; // Might skip somewhere, but not super important
         servo_cycles += 1;
     } while( (signed long)(next_match - OSCR) <= 8 );
-    //    write_sequnlock(&xtime_lock);
+    
+    // Send servo_valN to the appropriate PWM channel when the time is right.
     PWM_PWDUTY0 = ((servo_cycles & 0x3) == 0x3)?servo_val0:0;
     PWM_PWDUTY1 = ((servo_cycles & 0x3) == 0x3)?servo_val1:0;
 
@@ -110,6 +102,7 @@ static irqreturn_t servo_timer_handler(int irq, void *dev_id) {
 }
 
 static int pwmservo_init(void) {
+    /** Register character driver for pwmservo **/
     int ret;
     ret = register_chrdev(PWMSERVO_MAJOR, "pwmservo", &pwmservo_fops);
 
@@ -118,6 +111,7 @@ static int pwmservo_init(void) {
         return ret;
     }
     
+    /** Setup GPIOs for PWM0 and PWM1 **/
     pxa_gpio_mode(GPIO16_PWM0_MD); // setup GPIO16 as PWM0
     pxa_set_cken(CKEN0_PWM0,1); //Enable the PWM0 Clock
     servo_val0 = 0;
@@ -125,9 +119,8 @@ static int pwmservo_init(void) {
     PWM_CTRL0 = 0x3F;
     PWM_PERVAL0 = 0x3FF;
 
-    pxa_gpio_mode(GPIO17_PWM1_MD); // setup GPIO16 as PWM0
-    pxa_set_cken(CKEN1_PWM1,1); //Enable the PWM0 Clock
-
+    pxa_gpio_mode(GPIO17_PWM1_MD); // setup GPIO17 as PWM1
+    pxa_set_cken(CKEN1_PWM1,1); //Enable the PWM1 Clock
     servo_val1 = 0;
     PWM_PWDUTY1 = servo_val1;
     PWM_CTRL1 = 0x3F;
@@ -135,7 +128,6 @@ static int pwmservo_init(void) {
     
     // Initialize OS TIMER
     //    OSSR = 0xf; /* clear status on all timers */
-    //    OSSR = 0xf
     if (request_irq(IRQ_OST1, &servo_timer_handler, IRQF_TIMER | IRQF_DISABLED, "", NULL)) {
         goto fail;
     }
@@ -144,8 +136,6 @@ static int pwmservo_init(void) {
     // Install OS Timer Match 1 interrupt
     OSMR1 = OSCR + TOGGLE_TICK; /* set initial match */
 
-    //    pwm_servo_init(&pan_servo, 16, 10);
-    //    pwm_servo_init(&tilt_servo, 17, 10);
     printk(KERN_ALERT "Installed PWM Servo");
     return 0;
 fail: 
@@ -158,8 +148,9 @@ static void pwmservo_exit(void) {
 	/* Free memory */	
     unregister_chrdev(PWMSERVO_MAJOR, "pwmservo");
 
+    // Disable and release PWM0 and PWM1 clocks
     pxa_set_cken(CKEN0_PWM0,0); //Enable the PWM0 Clock
-    pxa_set_cken(CKEN1_PWM1,0); //Enable the PWM0 Clock
+    pxa_set_cken(CKEN1_PWM1,0); //Disable the PWM1 Clock
 
     OIER = OIER & (~OIER_E1); /* disable match on timer match 1 to cause interrupts */
     free_irq(IRQ_OST1, NULL);
@@ -185,6 +176,7 @@ static int pwmservo_release(struct inode *inode, struct file *filp)
 static ssize_t pwmservo_read( struct file *filp, char *buf, size_t count, 
                             loff_t *f_pos )
 {
+    /* success... kind of */
     return 0;
 }
 
@@ -198,17 +190,21 @@ static ssize_t pwmservo_write(struct file *filp, const char *buf, size_t count,
          return -EFAULT; 
      }
 
+     // Read input, only cares about tNUM and pNUM formatted inputs for controlling pan and tilt
      if (input_buffer[0] == 't') {
+         // Set tilt servo value, only accept numbers between 200 and 410. All other numbers
+         // may cause damage to the servo.
          res = simple_strtol((input_buffer+1), NULL, 10);
          if (((res > 199) && (res < 411)) || (res == 0)){ // Between 200 and 410
-             //             PWM_PWDUTY0 = res;
              servo_val0 = res;
          }
      } else if (input_buffer[0] =='p') {
+         // Set pan servo value, only accept numbers between 200 and 410. All other numbers
+         // may cause damage to the servo by driving it to a location it is mechanically stopped
+         // from and stripping the gears.
          res = simple_strtol((input_buffer+1), NULL, 10);
          if (((res > 199) && (res < 411)) || (res == 0)) { // Between 200 and 410
              servo_val1 = res;
-             //             PWM_PWDUTY1 = res;
          }
      } else {
          return -EFAULT;
